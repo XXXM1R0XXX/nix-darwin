20d89db603d9bcb05694c176e51e28e8543e8893
m1r0 <kristianbogdan8519@gmail.com>
2025-11-19 22:07:03 +0300

    Refactor git configuration structure; move user settings into a dedicated section and clean up deprecated fields

--- home/git.nix
+++ home/git.nix
@@ -16,10 +16,6 @@
     enable = true;
     lfs.enable = true;
 
-    # TODO replace with your own name & email
-    userName = username;
-    userEmail = useremail;
-
     includes = [
       {
         # use diffrent email & name for work
@@ -28,39 +24,42 @@
       }
     ];
 
-    extraConfig = {
+    # Все настройки теперь живут внутри settings
+    settings = {
+      # Замена userName и userEmail
+      user = {
+        name = username;
+        email = useremail;
+      };
+
+      # Бывший extraConfig
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
-    };
 
-    # signing = {
-    #   key = "xxx";
-    #   signByDefault = true;
-    # };
-
-    delta = {
-      enable = true;
-      options = {
-        features = "side-by-side";
+      # Бывшие aliases
+      alias = {
+        br = "branch";
+        co = "checkout";
+        st = "status";
+        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+        cm = "commit -m";
+        ca = "commit -am";
+        dc = "diff --cached";
+        amend = "commit --amend -m";
+        update = "submodule update --init --recursive";
+        foreach = "submodule foreach";
       };
     };
+  };
 
-    aliases = {
-      # common aliases
-      br = "branch";
-      co = "checkout";
-      st = "status";
-      ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
-      ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
-      cm = "commit -m";
-      ca = "commit -am";
-      dc = "diff --cached";
-      amend = "commit --amend -m";
-
-      # aliases for submodule
-      update = "submodule update --init --recursive";
-      foreach = "submodule foreach";
+  # Delta теперь настраивается отдельно, вне programs.git
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      features = "side-by-side";
     };
   };
 }

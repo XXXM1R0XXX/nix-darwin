ca3dfde93c8b29879599d822eed5200b49ad94b9
Your Name <you@example.com>
2025-11-19 18:11:11 +0300

    Update flake.lock and starship configuration; add wallpaper image

--- home/starship.nix
+++ home/starship.nix
@@ -6,20 +6,20 @@
     enableZshIntegration = true;
     enableNushellIntegration = true;
 
-    settings = {
-      character = {
-        success_symbol = "[›](bold green)";
-        error_symbol = "[›](bold red)";
-      };
-      aws = {
-        symbol = "🅰 ";
-      };
-      gcloud = {
-        # do not show the account/project's info
-        # to avoid the leak of sensitive information when sharing the terminal
-        format = "on [$symbol$active(\($region\))]($style) ";
-        symbol = "🅶 ️";
-      };
-    };
+  #   settings = {
+  #     character = {
+  #       success_symbol = "[›](bold green)";
+  #       error_symbol = "[›](bold red)";
+  #     };
+  #     aws = {
+  #       symbol = "🅰 ";
+  #     };
+  #     gcloud = {
+  #       # do not show the account/project's info
+  #       # to avoid the leak of sensitive information when sharing the terminal
+  #       format = "on [$symbol$active(\($region\))]($style) ";
+  #       symbol = "🅶 ️";
+  #     };
+  #   };
   };
 }
